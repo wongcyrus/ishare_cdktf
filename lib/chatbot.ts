4a33beb10bc575f5a25669212f6d510714b25eb9
Cyrus <cywong@vtc.edu.hk>
2021-09-14 23:25:12 -0400

    Add null resource to save Chat Bot key to file.

--- lib/chatbot.ts
+++ lib/chatbot.ts
@@ -3,9 +3,11 @@ import {
     BotChannelsRegistration,
     BotChannelWebChat,
     DataAzurermClientConfig,
-    ResourceGroup
+    ResourceGroup,
 } from "@cdktf/provider-azurerm";
 import {Application} from "../.gen/providers/azuread/application";
+import {Resource} from "@cdktf/provider-null";
+
 
 interface ChatBotConstructConstructProps {
     resourceGroup: ResourceGroup;
@@ -20,6 +22,7 @@ export class ChatBotConstruct extends Construct {
         super(scope, name);
 
         const {resourceGroup} = props;
+        const resourceGroupName = resourceGroup.name;
 
         const dataAzureRmClientConfig = new DataAzurermClientConfig(this, "Client Config");
         const application = new Application(this, "iShare Bot Application", {
@@ -27,28 +30,34 @@ export class ChatBotConstruct extends Construct {
             owners: [dataAzureRmClientConfig.objectId],
         });
 
-        // new BotWebApp(this, "iShare BotWebApp", {
-        //     microsoftAppId: application.objectId,
-        //     name: process.env.STORAGE_ACCOUNT_NAME! + process.env.ENV + "BotWebApp",
-        //     resourceGroupName: resourceGroup.name,
-        //     location: resourceGroup.location,
-        //     sku: process.env.BOTWEBAPP_SKU!
-        // })
-
         const botChannelsRegistration = new BotChannelsRegistration(this, "iShare BotChannelsRegistration", {
             microsoftAppId: application.objectId,
             name: process.env.STORAGE_ACCOUNT_NAME! + process.env.ENV + "BotWebApp",
-            resourceGroupName: resourceGroup.name,
+            resourceGroupName,
             location: "global",
             sku: process.env.BOTWEBAPP_SKU!
         });
 
-        new BotChannelWebChat(this, "iShare BotChannelWebChat", {
-            botName: botChannelsRegistration.name,
-            resourceGroupName: resourceGroup.name,
+        const botName = botChannelsRegistration.name;
+        const botChannelWebChat = new BotChannelWebChat(this, "iShare BotChannelWebChat", {
+            botName,
+            resourceGroupName,
             location: "global",
             siteNames: ["donor", "volunteer", "student", "teacher", "anonymous"]
         });
+
+        const nullResource = new Resource(this, "Bot Channel WebChat Keys", {
+            triggers: {
+                dummy: new Date().getMilliseconds().toString()
+            },
+            dependsOn: [botChannelWebChat],
+        });
+        nullResource.addOverride(
+            "provisioner.local-exec.command", `az bot webchat show --name ${botName} --resource-group ${resourceGroupName} --with-secrets \
+            | jq '.resource.properties.sites | map({siteName:.siteName, key:.key})' \
+            > ../../../webchat_secrets.json`
+        );
+
 
     }
 }
