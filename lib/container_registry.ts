aa45c701475ee0c5dc85b92d3c709fd660a95f41
hades <200187164@stu.vtc.edu.hk>
2021-11-04 04:34:51 -0400

    add command for docker to login azurecr, build and push image

--- lib/container_registry.ts
+++ lib/container_registry.ts
@@ -1,6 +1,6 @@
 import {Construct} from "constructs";
 import {ContainerRegistry, ResourceGroup} from "@cdktf/provider-azurerm";
-
+import {Resource} from "@cdktf/provider-null";
 interface ContainerRegistryConstructProps {
     resourceGroup: ResourceGroup;
 }
@@ -30,6 +30,36 @@ export class ContainerRegistrySConstruct extends Construct {
                 dependsOn: [resourceGroup],
                 tags: JSON.parse(process.env.TAG!),
             }
+        );
+        const dockerloginNullResource = new Resource(this, "login to azurecr",{
+            triggers: {
+                dummy: new Date().getMilliseconds().toString()
+            }
+        });
+        const dockerbuilddockerNullResource = new Resource(this, "build docker image", {
+            triggers: {
+                dummy: new Date().getMilliseconds().toString()
+            },
+            dependsOn: [dockerloginNullResource],
+        });
+        const dockerpushNullResource = new Resource(this, "push to azurecr", {
+            triggers: {
+                dummy: new Date().getMilliseconds().toString()
+            },
+            dependsOn: [dockerbuilddockerNullResource, dockerloginNullResource],
+        });
+        const serverentry = this.containerRegistry.loginServer;
+        const username = this.containerRegistry.adminUsername;
+        const password = this.containerRegistry.adminPassword;
+        const dockerlocation = process.env.DOCKERFILESLOCATION!;
+        dockerloginNullResource.addOverride(
+            "provisioner.local-exec.command",`docker login ${serverentry} -u ${username} -p ${password}`
+        );
+        dockerbuilddockerNullResource.addOverride(
+            "provisioner.local-exec.command", `docker build -t ${serverentry}/pc_donation:v0.0.0 ${dockerlocation}`
+        );
+        dockerpushNullResource.addOverride(
+            "provisioner.local-exec.command", `docker push ${serverentry}/pc_donation:v0.0.0`
         );
     }
 }
