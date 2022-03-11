95c0b2a5566b454d80eac838ffe20bad2a7681dc
hades <hmchan10@hotmail.com>
2022-03-11 16:58:14 -0500

    changed to use az-cli to deploy app service due to unable to deploy with provider AzureRM

--- lib/app_service.ts
+++ lib/app_service.ts
@@ -1,21 +1,48 @@
 import {Construct} from "constructs";
-import {AppService, AppServicePlan, MysqlServer, ResourceGroup,} from "@cdktf/provider-azurerm";
-
+import {
+    //AppService,
+    AppServicePlan,
+    //AppServiceSiteCredential,
+    MysqlServer,
+    ResourceGroup,
+} from "@cdktf/provider-azurerm";
+import {ContainerRegistrySConstruct} from "./container_registry";
+import {Resource} from "@cdktf/provider-null";
 interface AppServiceConstructProps {
     resourceGroup: ResourceGroup;
     appServicePlan: AppServicePlan;
     mysqlServer: MysqlServer;
+    containerregistry: ContainerRegistrySConstruct;
 }
 
 export class AppServiceConstruct extends Construct {
-    public readonly appService: AppService;
+    //public readonly appService: AppService;
 
     constructor(scope: Construct, name: string, props: AppServiceConstructProps) {
         super(scope, name);
 
-        const {resourceGroup, appServicePlan} = props;
+        const {resourceGroup, appServicePlan, containerregistry} = props;
+        const App_Service_Creation = new Resource(this, "create app service",{
+            triggers: {
+                dummy: new Date().getMilliseconds().toString()
+            },
+            dependsOn: [appServicePlan, containerregistry.dockerbuild]
+        });
+        const appname = process.env.PROJECT_NAME! + process.env.ENV;
+        const resourcegroup = resourceGroup.name;
+        const appServicePlanId = appServicePlan.id;
+        const imagename = props.containerregistry.containerRegistry.loginServer + "/" + process.env.PROJECT_NAME
+        const acr_user = props.containerregistry.containerRegistry.adminUsername;
+        const acr_pass = props.containerregistry.containerRegistry.adminPassword;
+
+        App_Service_Creation.addOverride(
+            "provisioner.local-exec.command",` sleep 30 && branch=$(git symbolic-ref --short HEAD) && hash=$(git rev-parse --short HEAD) &&  \ 
+            az webapp create --resource-group ${resourcegroup} --plan ${appServicePlanId} --name ${appname} --deployment-container-image-name ${imagename}-$branch:$hash --docker-registry-server-user ${acr_user} && \ 
+            az webapp config appsettings set --resource-group ${resourcegroup} --name ${appname} --settings WEBSITES_PORT=5000 DOCKER_REGISTRY_SERVER_PASSWORD="${acr_pass}"`
+        );
 
         // TODO: Cannot get principalId using DataAzurermMysqlServer
+        /*const test = new DataAzurermMysqlServer(this, "test", {
         /*const test = new DataAzurermMysqlServer(this, "test", {
               name: mysql_server.name,
               resourceGroupName: app_rg.name,
@@ -26,17 +53,31 @@ export class AppServiceConstruct extends Construct {
 
         // create MySQL Database
         // https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/app_service
-        this.appService = new AppService(this, "iShare App", {
+        /*this.appService = new AppService(this, "iShare App", {
             name: process.env.PROJECT_NAME! + process.env.ENV,
             resourceGroupName: resourceGroup.name,
             location: resourceGroup.location,
             appServicePlanId: appServicePlan.id,
-            // identity: [
-            //   {
-            //     type: process.env.APP_SERVICE_IDENTITY_TYPE!,
-            //   },
-            // ],
-            dependsOn: [appServicePlan],
-        });
+            /*identity: [
+                {
+                    type: process.env.APP_SERVICE_IDENTITY_TYPE!,
+                },
+            ],
+            dependsOn: [appServicePlan, containerregistry.containerRegistry],
+            /*appSettings: {
+                "WEBSITES_PORT" : "5000",
+                "DOCKER_REGISTRY_SERVER_URL" : props.containerregistry.containerRegistry.loginServer,
+                "DOCKER_REGISTRY_SERVER_USERNAME" : props.containerregistry.containerRegistry.adminUsername,
+                "DOCKER_REGISTRY_SERVER_PASSWORD" : props.containerregistry.containerRegistry.adminPassword,
+            },
+            tags: JSON.parse(process.env.TAG!),
+            kind: "linux",
+            siteConfig: {
+                linuxFxVersion: "DOCKER|isharedemotest3dev.azurecr.io/isharedemotest3-hades:2fb17de",
+                always_on: true,
+                health_check_path: "/health",
+            }
+
+        });*/
     }
 }
