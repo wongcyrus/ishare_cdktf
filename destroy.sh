84a46177e6c5486f05fb52c067d79dd269be6be8
hades <hmchan10@hotmail.com>
2022-03-13 07:37:36 -0400

    improve the code for destroy resources

--- destroy.sh
+++ destroy.sh
@@ -1,6 +1,10 @@
-STACK_OUTPUT=./stack_output.json && export Appservicename=$(cat $STACK_OUTPUT  | jq -r .AppServicePlanName.value)
+#!/usr/bin/env bash
+
+PROJECT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $PROJECT_DIR
+export $(grep PROJECT_NAME= dev.env)
 export $(grep RESOURCE_GROUP_NAME= dev.env)
-echo "Are you confirm to stop and delete $Appservicename on Azure that created by cdktf? Please select with the numbers."
+echo "Are you confirm to stop and delete ${PROJECT_NAME}dev on Azure that created by cdktf? Please select with the numbers."
 select yn in "Yes" "No"; do
   case $yn in
     Yes ) break;;
@@ -8,5 +12,5 @@ select yn in "Yes" "No"; do
   esac
 done
 echo "Deleting..."
-az webapp delete --name $Appservicename --resource-group ${RESOURCE_GROUP_NAME}dev --verbose
+az webapp delete --name ${PROJECT_NAME}dev --resource-group ${RESOURCE_GROUP_NAME}dev --verbose
 cdktf destroy --auto-approve
