2364439f86cef9665afb1adc33f6a0f810e9b75b
hades-linux <hmchan10@hotmail.com>
2022-03-15 19:25:09 -0400

    new problem occurred by using AppService on AzureRM to create app service, revert to use az command until solution is found.

--- lib/app_service.ts
+++ lib/app_service.ts
@@ -1,13 +1,13 @@
 import {Construct} from "constructs";
 import {
-    AppService,
+    //AppService,
     AppServicePlan,
     //AppServiceSiteCredential,
     ResourceGroup,
 } from "@cdktf/provider-azurerm";
 import {ContainerRegistrySConstruct} from "./container_registry";
 import {Resource} from "@cdktf/provider-null";
-import {DataLocalFile} from "../.gen/providers/local/data-local-file";
+//import {DataLocalFile} from "../.gen/providers/local/data-local-file";
 interface AppServiceConstructProps {
     resourceGroup: ResourceGroup;
     appServicePlan: AppServicePlan;
@@ -15,15 +15,29 @@ interface AppServiceConstructProps {
 }
 
 export class AppServiceConstruct extends Construct {
-    public readonly appService: AppService;
+    //public readonly appService: AppService;
     constructor(scope: Construct, name: string, props: AppServiceConstructProps) {
         super(scope, name);
         const {resourceGroup, appServicePlan, containerregistry} = props;
+        const App_Service_Creation = new Resource(this, "get git branch and hash",{
+            triggers: {
+            },
+            dependsOn: [appServicePlan, containerregistry.dockerbuild]
+        });
         const imagename = props.containerregistry.containerRegistry.loginServer + "/" + process.env.PROJECT_NAME
+        const appname = process.env.PROJECT_NAME! + process.env.ENV;
+        const resourcegroup = resourceGroup.name;
+        const appServicePlanId = appServicePlan.id;
+        const acr_user = props.containerregistry.containerRegistry.adminUsername;
+        const acr_pass = props.containerregistry.containerRegistry.adminPassword;
 
+        App_Service_Creation.addOverride(
+            "provisioner.local-exec.command",` sleep 30 && branch=$(git symbolic-ref --short HEAD) && hash=$(git rev-parse --short HEAD) &&  \ 
+            az webapp create --resource-group ${resourcegroup} --plan ${appServicePlanId} --name ${appname} --deployment-container-image-name ${imagename}-$branch:$hash --docker-registry-server-user ${acr_user} && \ 
+            az webapp config appsettings set --resource-group ${resourcegroup} --name ${appname} --settings WEBSITES_PORT=5000 DOCKER_REGISTRY_SERVER_PASSWORD="${acr_pass}"`)
         // create MySQL Database
         // https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/app_service
-        const get_var = new Resource(this, "get git branch and hash",{
+        /*const get_var = new Resource(this, "get git branch and hash",{
             triggers: {
             },
             dependsOn: [appServicePlan, containerregistry.dockerbuild]
@@ -50,7 +64,7 @@ export class AppServiceConstruct extends Construct {
                 {
                     type: process.env.APP_SERVICE_IDENTITY_TYPE!,
                 },
-            ]*/
+            ]
             dependsOn: [appServicePlan, containerregistry.containerRegistry],
             appSettings: {
                 "WEBSITES_PORT" : "5000",
@@ -62,10 +76,9 @@ export class AppServiceConstruct extends Construct {
             siteConfig: [{
                 linuxFxVersion: "DOCKER|"+ imagename + "-" + branch.content + ":" + hash.content,
                 alwaysOn: true,
-                healthCheckPath: "/health",
 
             }]
 
-        });
+        });*/
     }
 }
