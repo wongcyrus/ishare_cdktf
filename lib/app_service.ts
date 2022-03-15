5e32c7201d608822d5f87d3fcb4815c0c5086d1a
hades <hmchan10@hotmail.com>
2022-03-15 12:48:06 -0400

    able to use terraform to create App Service now

--- lib/app_service.ts
+++ lib/app_service.ts
@@ -1,12 +1,13 @@
 import {Construct} from "constructs";
 import {
-    //AppService,
+    AppService,
     AppServicePlan,
     //AppServiceSiteCredential,
     ResourceGroup,
 } from "@cdktf/provider-azurerm";
 import {ContainerRegistrySConstruct} from "./container_registry";
 import {Resource} from "@cdktf/provider-null";
+import {DataLocalFile} from "../.gen/providers/local/data-local-file";
 interface AppServiceConstructProps {
     resourceGroup: ResourceGroup;
     appServicePlan: AppServicePlan;
@@ -14,32 +15,33 @@ interface AppServiceConstructProps {
 }
 
 export class AppServiceConstruct extends Construct {
-    //public readonly appService: AppService;
+    public readonly appService: AppService;
     constructor(scope: Construct, name: string, props: AppServiceConstructProps) {
         super(scope, name);
         const {resourceGroup, appServicePlan, containerregistry} = props;
-        const App_Service_Creation = new Resource(this, "create app service",{
-            triggers: {
-                dummy: new Date().getMilliseconds().toString()
-            },
-            dependsOn: [appServicePlan, containerregistry.dockerbuild]
-        });
-        const appname = process.env.PROJECT_NAME! + process.env.ENV;
-        const resourcegroup = resourceGroup.name;
-        const appServicePlanId = appServicePlan.id;
         const imagename = props.containerregistry.containerRegistry.loginServer + "/" + process.env.PROJECT_NAME
-        const acr_user = props.containerregistry.containerRegistry.adminUsername;
-        const acr_pass = props.containerregistry.containerRegistry.adminPassword;
-
-        App_Service_Creation.addOverride(
-            "provisioner.local-exec.command",` sleep 30 && branch=$(git symbolic-ref --short HEAD) && hash=$(git rev-parse --short HEAD) &&  \ 
-            az webapp create --resource-group ${resourcegroup} --plan ${appServicePlanId} --name ${appname} --deployment-container-image-name ${imagename}-$branch:$hash --docker-registry-server-user ${acr_user} && \ 
-            az webapp config appsettings set --resource-group ${resourcegroup} --name ${appname} --settings WEBSITES_PORT=5000 DOCKER_REGISTRY_SERVER_PASSWORD="${acr_pass}"`
-        );
 
         // create MySQL Database
         // https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/app_service
-        /*this.appService = new AppService(this, "iShare App", {
+        const get_var = new Resource(this, "get git branch and hash",{
+            triggers: {
+            },
+            dependsOn: [appServicePlan, containerregistry.dockerbuild]
+        });
+        get_var.addOverride(
+            "provisioner.local-exec.command", `branch=$(git symbolic-ref --short HEAD) && echo -n $branch > ../../../git_branch && hash=$(git rev-parse --short HEAD) && echo -n $hash > ../../../git_hash `
+        );
+        const branch = new DataLocalFile(this, "branch of this Project in Github", {
+            filename: "../../../git_branch",
+            dependsOn: [get_var]
+        });
+        const hash = new DataLocalFile(this, "hash of this Project in Github", {
+            filename: "../../../git_hash",
+            dependsOn: [get_var]
+        });
+
+
+        this.appService = new AppService(this, "iShare App", {
             name: process.env.PROJECT_NAME! + process.env.ENV,
             resourceGroupName: resourceGroup.name,
             location: resourceGroup.location,
@@ -48,22 +50,22 @@ export class AppServiceConstruct extends Construct {
                 {
                     type: process.env.APP_SERVICE_IDENTITY_TYPE!,
                 },
-            ],
+            ]*/
             dependsOn: [appServicePlan, containerregistry.containerRegistry],
-            /*appSettings: {
+            appSettings: {
                 "WEBSITES_PORT" : "5000",
                 "DOCKER_REGISTRY_SERVER_URL" : props.containerregistry.containerRegistry.loginServer,
                 "DOCKER_REGISTRY_SERVER_USERNAME" : props.containerregistry.containerRegistry.adminUsername,
                 "DOCKER_REGISTRY_SERVER_PASSWORD" : props.containerregistry.containerRegistry.adminPassword,
             },
             tags: JSON.parse(process.env.TAG!),
-            kind: "linux",
-            siteConfig: {
-                linuxFxVersion: "DOCKER|isharedemotest3dev.azurecr.io/isharedemotest3-hades:2fb17de",
-                always_on: true,
-                health_check_path: "/health",
-            }
+            siteConfig: [{
+                linuxFxVersion: "DOCKER|"+ imagename + "-" + branch.content + ":" + hash.content,
+                alwaysOn: true,
+                healthCheckPath: "/health",
 
-        });*/
+            }]
+
+        });
     }
 }
